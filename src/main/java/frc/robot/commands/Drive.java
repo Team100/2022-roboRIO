91401e4ae208f32a8c963cbcf345769612164196
Nayan Smuek <44534701+NikolaTesla17@users.noreply.github.com>
2022-02-10 09:59:56 -0800

    started adding ramp up limiter function

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -26,13 +26,21 @@ public class Drive extends CommandBase {
     @Override
     public void initialize() {}
 
+    public double ramp(double motorOutput) {
+        double change = motorOutput - limitedJoystick;
+        if (change>Constants.DrivetrainConstants.DrivetrainControls.RAMP_LIMIT){
+            change = Constants.DrivetrainConstants.DrivetrainControls.RAMP_LIMIT;
+        }
+        else (if change<-limit) change = -limit;
+        limitedJoystick += change;
+        return limitedJoystick;
+    }
+
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        // double left = -leftJoystick.getY() + rightJoystick.getX();
-        // double right = -leftJoystick.getY() - rightJoystick.getX();
-        double left = leftJoystick.getY() - rightJoystick.getX();
-        double right = leftJoystick.getY() + rightJoystick.getX();
+        double left = ramp(leftJoystick.getY() - rightJoystick.getX());
+        double right = ramp(leftJoystick.getY() + rightJoystick.getX());
         double limiter = (-rightJoystick.getZ()+1)/2;
         drivetrain.set((left*limiter), (right*limiter));
     }
