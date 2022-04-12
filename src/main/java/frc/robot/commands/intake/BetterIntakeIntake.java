e38fd9b9233e556a3ab939c2d9ce133b9fc47969
Nayan Smuek <44534701+NikolaTesla17@users.noreply.github.com>
2022-04-12 15:51:39 -0700

    I HAVE THINGS TO DO

--- src/main/java/frc/robot/commands/intake/BetterIntakeIntake.java
+++ src/main/java/frc/robot/commands/intake/BetterIntakeIntake.java
@@ -30,17 +30,20 @@ public class BetterIntakeIntake extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        if (intake.getMeasurement() >= 20) {
-            intake.runPivot(Constants.IntakeConstants.IntakeMotionParameters.INTAKE_PIVOT_PERCENT_OUTPUT_DOWN);
-            intake.runSpinner(0);
-        } else if (intake.getMeasurement() >= 10) {
-            intake.runPivot(Constants.IntakeConstants.IntakeMotionParameters.INTAKE_PIVOT_PERCENT_OUTPUT_DOWN_DECEL);
-            intake.runSpinner(0);
-        } else {
-            intake.runPivot(0);
-            intake.runSpinner(Constants.IntakeConstants.IntakeMotionParameters.INTAKE_SPINNER_PERCENT_OUTPUT);
-            //intake.runSpinner(slowButton.get() ? Constants.IntakeConstants.IntakeMotionParameters.INTAKE_SPINNER_SLOW_PERCENT_OUTPUT : Constants.IntakeConstants.IntakeMotionParameters.INTAKE_SPINNER_PERCENT_OUTPUT);
-        }
+        // if (intake.getMeasurement() >= 20) {
+        //     intake.runPivot(Constants.IntakeConstants.IntakeMotionParameters.INTAKE_PIVOT_PERCENT_OUTPUT_DOWN);
+        //     intake.runSpinner(0);
+        // } else if (intake.getMeasurement() >= 10) {
+        //     intake.runPivot(Constants.IntakeConstants.IntakeMotionParameters.INTAKE_PIVOT_PERCENT_OUTPUT_DOWN_DECEL);
+        //     intake.runSpinner(0);
+        // } else {
+        //     intake.runPivot(0);
+        
+        //     intake.runSpinner(Constants.IntakeConstants.IntakeMotionParameters.INTAKE_SPINNER_PERCENT_OUTPUT);
+            // intake.runSpinner(slowButton.get() ? Constants.IntakeConstants.IntakeMotionParameters.INTAKE_SPINNER_SLOW_PERCENT_OUTPUT : Constants.IntakeConstants.IntakeMotionParameters.INTAKE_SPINNER_PERCENT_OUTPUT);
+        //}
+
+        intake.runSpinner(Constants.IntakeConstants.IntakeMotionParameters.INTAKE_SPINNER_PERCENT_OUTPUT);
     }
 
     // Called once the command ends or is interrupted.
